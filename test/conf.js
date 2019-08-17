ee0957e0bf73e58bfe6efd9bd9e28bb3a32fd1ad
wilson.calixto <wilson.calixto@grupoicts.com.br>
2019-08-17 12:35:51 -0400

    ssssss

--- test/conf.js
+++ test/conf.js
@@ -4,7 +4,7 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['Alert.spec.js','datepiecker.spec.js'],
+    specs: ['test_protractor_2.spec.js'],
 
     onPrepare: function(){
         jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
